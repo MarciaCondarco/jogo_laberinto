d48dc1153588a27c571444531ae3cda2d9aa4958
Marcy_ <marciagisseli21@gmail.com>
2025-04-03 18:45:37 -0300

    posicionando o player no começo do labirinto

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ canvas.height = 576
 
 contexto.fillStyle = 'green'
 // definindo a cor verde no espaço canva
-contexto.fillRect(  0, 0, canvas.width, canvas.height)
+contexto.fillRect(0, 0, canvas.width, canvas.height)
 // definindo a posição X e Y do canva
 
 const image = new Image()
 // criando uma variavel que contêm a imagem do mapa
 image.src = './mapajogo.png'
 
+const playerImage = new Image();
+playerImage.src = './playerdown.png'
+
 console.log(image)
 
 image.onload = () => {
-    contexto.drawImage(image, -10, -300)
-// definindo a posição X e Y da imagem do mapa do jogo
+    contexto.drawImage(image, 0 ,-900)
+    contexto.drawImage(playerImage, canvas.width/3, canvas.height/2)
+    // definindo a posição X e Y da imagem do mapa do jogo
 
 }
-// imagem do mapa do jogo vai carregar e chamar a variavel contexto   
+// imagem do mapa do jogo vai carregar e chamar a variavel contexto 
+
